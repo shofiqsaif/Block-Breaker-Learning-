590a3ac9e09eb22200f792c419717f053a1731a5
shofiqsaif <sislam161079@gmail.com>
2021-03-04 13:40:05 +0600

    7. Implemented Lose Collider

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LoseCollider : MonoBehaviour
+{
+    [SerializeField] SceneLoder SL; 
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SL.LoadScene("Gameover");
+        
+    }
+}
---
+++ Assets/Scripts/SceneLoder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoder : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadStartingScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void onPressQuit()
+    {
+        Application.Quit();
+    }
+
+    public void LoadScene(string s)
+    {
+        SceneManager.LoadScene(s);
+    }
+}
