881f840893ff8036651b20f09957fbbc9059776d
shofiqsaif <sislam161079@gmail.com>
2021-03-05 10:50:46 +0600

    13. Ball Launch and Play Spcae

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    //config Param
+    [SerializeField] Paddle paddle1;
+    [SerializeField] Vector2 launchVelocity;
+    
+    // States
+    Vector2 paddleToBallVector;
+    bool hasStarted = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallVector =  (transform.position - paddle1.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchBall();
+        }
+        
+    }
+
+    private void LaunchBall()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = launchVelocity;
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = paddle1.transform.position;
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidthInUnit = 16f;
+    [SerializeField] float minX;
+    [SerializeField] float maxX;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosInUnit= (Input.mousePosition.x / Screen.width * screenWidthInUnit);
+        
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        mousePosInUnit = Mathf.Clamp(mousePosInUnit, minX, maxX);
+        paddlePos.x = mousePosInUnit;
+
+        transform.position = paddlePos;
+
+    }
+}
